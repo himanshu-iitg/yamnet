06626c299bcd16ead8e195a4c26da692e5d812a9
himanshu-iitg <119754705+himanshu-iitg@users.noreply.github.com>
2023-05-18 22:25:33 +0530

    convert model to function call and fix sample rate in constants

---
+++ config/constants.py
@@ -0,0 +1,5 @@
+import os
+
+MODEL_PATH = os.path.join(os.path.dirname(__file__), '../yamnet/yamnet.h5')
+CLASS_MAP_PATH = os.path.join(os.path.dirname(__file__), '../yamnet/yamnet_class_map.csv')
+FS = 16000
---
+++ run.py
@@ -0,0 +1,26 @@
+import numpy as np
+import scipy as scipy
+
+from yamnet import yamnet_predictor, class_names
+from config.constants import FS
+
+
+def ensure_sample_rate(original_sample_rate, waveform,
+                       desired_sample_rate=FS):
+    """Resample waveform if required."""
+    if original_sample_rate != desired_sample_rate:
+        desired_length = int(round(float(len(waveform)) /
+                                   original_sample_rate * desired_sample_rate))
+        waveform = scipy.signal.resample(waveform, desired_length)
+    return desired_sample_rate, waveform
+
+
+def get_audio_scores(fs, audio, top_n = 10):
+    fs, audio = ensure_sample_rate(fs, audio)
+
+    scores, embeddings, spectrogram = yamnet_predictor(audio)
+    scores = scores.numpy()
+    mean_scores = np.mean(scores, axis=0)
+    top_class_indices = np.argsort(mean_scores)[::-1][:top_n]
+    yticks = range(0, top_n, 1)
+    return [(top_class_indices[x], class_names[top_class_indices[x]], mean_scores[top_class_indices[x]]) for x in yticks]
