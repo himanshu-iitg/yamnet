a5e164d122bc068628d8deae9ced74e355275f54
himanshu-iitg <119754705+himanshu-iitg@users.noreply.github.com>
2023-05-20 12:30:38 +0530

    add hop seconds in constants

--- yamnet/config/constants.py
+++ yamnet/config/constants.py
@@ -3,3 +3,4 @@ import os
 MODEL_PATH = os.path.join(os.path.dirname(__file__), '../soundpredictor/yamnet.h5')
 CLASS_MAP_PATH = os.path.join(os.path.dirname(__file__), '../soundpredictor/yamnet_class_map.csv')
 FS = 16000
+HOP_SECONDS = 0.1
